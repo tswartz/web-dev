13f47016056147c47528ccad2207d11795e09e37
tswartz <tswartz92@gmail.com>
2015-02-13 16:02:34 -0500

    Delete's working, doing edit now

--- essential/assignments/jquery/script.js
+++ essential/assignments/jquery/script.js
@@ -4,8 +4,14 @@ var courses = [
 ];
 
 $(function () {
+	renderCourses(courses);
+	$('#courses-created').on('click', 'button.remove', removeCourse);
+	$('#courses-created').on('click', 'button.edit', editCourse);
+});
+
+function renderCourses(courses) {
 	var tbody = $('#courses-created');
-	var template = $('.template');
+	var template = $( $('.template').get(0) );
 	tbody.empty();
 
 	courses.map(function (course, index) {
@@ -16,8 +22,49 @@ $(function () {
 				td.html(course[prop]);
 			}
 		};
+		clonedTemplate.find('.edit').attr('id', index);
+		clonedTemplate.find('.remove').attr('id', index);
 		tbody.append(clonedTemplate);
 	});
-});
+}
+
+function removeCourse(e) {
+	bootbox.confirm({
+		title: "Delete Course",
+	  	message: "Are you sure you want to delete this course?",
+	  	callback: function(result) {
+	  		if (result) {
+				var removeButton = $(e.currentTarget);
+				var id = removeButton.attr('id')
+				courses.splice(id, 1);
+				renderCourses(courses);
+	  		}
+	  	}
+	});
+}
+
+function editCourse() {
+	bootbox.dialog({
+		title: "Create New Course",
+	  	message: $('#add-edit-dialog').html(),
+	  	buttons: [
+	  		{
+		    	label: "Cancel",
+		    },
+		    {
+		    	label: "OK",
+		      	className: "btn-primary",
+		      	callback: function(e) {
+		      		console.log($('.modal-body #add-edit-form').find('input'));
+		        	console.log('great success');
+		      	}
+		    },
+	  	]
+	});
+}
+
+function openNewCourseDialog(form) {
+	
+}
 
 
