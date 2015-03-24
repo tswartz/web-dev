1198de022f6114faa05516df246f29cd801abc40
tswartz <tswartz92@gmail.com>
2015-03-24 16:56:48 -0400

    Added documentation and cleanup

--- essential/assignments/angular+node/public/controller.js
+++ essential/assignments/angular+node/public/controller.js
@@ -4,6 +4,7 @@ app.controller("OnlineUniversityController",
 function ($scope, $http)
 {
 
+	// opens modal to edit course (populates inputs with clicked course)
 	$scope.openEditCourseDialog = function(course, index) {
 		$scope.submitCourse = $scope.editCourse;
 		$scope.editIndex = index;
@@ -12,6 +13,7 @@ function ($scope, $http)
 		$('#courseModal').modal('show');
 	};
 
+	// opens modal to add a course (populates dateCreated with today's date)
 	$scope.openAddCourseDialog = function() {
 		$scope.submitCourse = $scope.addCourse;
 		if ($scope.form) {
@@ -23,6 +25,7 @@ function ($scope, $http)
 		$('#courseModal').modal('show');
 	};
 
+	// updates course at $scope.editIndex
     $scope.editCourse = function(updatedCourse) {
     	if ($scope.form.$invalid) {
     		return;
@@ -34,6 +37,7 @@ function ($scope, $http)
 		});
     };
 
+    // adds course
     $scope.addCourse = function(newCourse) {
     	if ($scope.form.$invalid) {
     		return;
@@ -45,6 +49,7 @@ function ($scope, $http)
 		});
     };
 
+    // removes course at index
     $scope.removeCourse = function(course, index) {
     	$http.delete('/api/course/' + index).
 		success(function(response) {
@@ -52,6 +57,7 @@ function ($scope, $http)
 		});
     };
 
+    // formats dateCreated value to be more human-readable
     $scope.formatDate = function(date) {
 		date = new Date(date);
 		var month = twoDigitFormat(date.getMonth()+1);
@@ -59,10 +65,13 @@ function ($scope, $http)
 		return month + '/' + day + '/' + date.getFullYear();
 	};
 
+	// formats single digit numbers with leading zero for dates
+	// ex: twoDigitFormat(1) -> "01"
 	twoDigitFormat = function(n) {
 		return n >= 10 ? n : "0" + n;
 	};
 
+	// Initial retrieval of courses
     $http.get("/api/courses")
     .success( function(response) {
     	$scope.courses = response;
